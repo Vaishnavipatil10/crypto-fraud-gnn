1b7f2e5e02098334b3511f691ed7f7781385c126
Vaishnavipatil10 <89210997+Vaishnavipatil10@users.noreply.github.com>
2026-04-07 00:09:34 +0530

    Add files via upload

---
+++ utils/model/gnn_model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+
+class GNN(torch.nn.Module):
+    def __init__(self, input_dim):
+        super(GNN, self).__init__()
+        self.conv1 = GCNConv(input_dim, 64)
+        self.conv2 = GCNConv(64, 2)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
