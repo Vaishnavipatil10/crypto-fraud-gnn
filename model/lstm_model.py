461a16534fc2d38efe16f659d5bee698facce329
Vaishnavipatil10 <89210997+Vaishnavipatil10@users.noreply.github.com>
2026-04-07 18:39:57 +0530

    Update lstm_model.py

---
+++ model/lstm_model.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+class LSTMModel(nn.Module):
+    def __init__(self):
+        super(LSTMModel, self).__init__()
+        self.lstm = nn.LSTM(input_size=1, hidden_size=50, batch_first=True)
+        self.fc = nn.Linear(50, 1)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = self.fc(out[:, -1, :])
+        return out
